f1d4537780238a6a8479bcd174b4669a8f8b1668
D4rULi3b3rt <122429060+khacminh03@users.noreply.github.com>
2024-01-05 16:51:04 +0700

    Update leetcode-206.py

---
+++ leetcode/leetcode-206.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        current = head
+        while(current):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        return prev
+        
